2ee1c93d8faff3cf4ba237236c3bc24c4d97da7c
Raphael Anaximenes <77770101+RaphaelAnaximenes@users.noreply.github.com>
2023-09-03 18:28:35 -0300

    test: ensure that a valid bet is acceptable (and charged from wallet)

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -6,11 +6,11 @@ describe('User tests', () => {
     let user: User<UserData>;
 
     beforeEach(() => {
-        user = new User('John', 100);
+        user = new User('XuxaDaSilva', 100);
     });
 
     it('ensure User initializes with a name, balance, and a wallet of 50 bucks', () => {
-        expect(user.name).toEqual('John');
+        expect(user.name).toEqual('XuxaDaSilva');
         expect(user.balance).toEqual(100);
         expect(user.wallet).toEqual(50);
     });
@@ -21,6 +21,14 @@ describe('User tests', () => {
         expect(user.balance).toEqual(150); 
         expect(user.wallet).toEqual(0); 
     });
+    
+    it('ensure that a valid bet is acceptable (and charged from wallet)', () => {
+        User.setBet(user, 50);
+        expect(user.balance).toEqual(50); 
+        expect(user.currentBet).toEqual(50);
+    });
+
+
 
 
 });
